943b1ff54ad48c9c6a2a5ce9bf5ef70f2fe072e1
Shauvon McGill <shauvon.mcgill@gmail.com>
2020-07-12 19:05:39 -0700

    Another config to use Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  server: 'https://server-dot-denyconformity.uc.r.appspot.com'
+  // server: 'https://server-dot-denyconformity.uc.r.appspot.com'
+  server: 'https://denyconformity-server.herokuapp.com/'
 };
