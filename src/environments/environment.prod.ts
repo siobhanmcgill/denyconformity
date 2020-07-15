c782d07626469010f3c02ccc9212d8588cd5d894
Shauvon McGill <shauvon.mcgill@gmail.com>
2020-07-14 22:12:24 -0700

    Just kidding. This is really what will work.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // server: 'https://server-dot-denyconformity.uc.r.appspot.com'
-  server: 'https://denyconformity-server.herokuapp.com/'
+  server: 'https://denyconformity-server.herokuapp.com'
 };
